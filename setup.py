c6517225532892c8d36092a49dbaa6ab99c98716
Ratnadeep Debnath <rtnpro@gmail.com>
2015-08-04 01:04:15 +0530

    Updated setup.py to read description from README.md.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='ircb',
     version='0.0.1',
     description='A IRC bouncer',
-    long_description=''.join(open('README.rst').readlines()),
+    long_description=''.join(open('README.md').readlines()),
     keywords='some, keywords',
     author='Ratnadeep Debnath',
     author_email='rtnpro@gmail.com',
