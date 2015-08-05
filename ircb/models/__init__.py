873d576eca2ca8a862ebd6ea8169ebb1ecd9ce99
Ratnadeep Debnath <rtnpro@gmail.com>
2015-08-05 18:37:31 +0530

    Added Channel model.

--- ircb/models/__init__.py
+++ ircb/models/__init__.py
@@ -1,5 +1,6 @@
 from .network import Network
 from .user import User
+from .channel import Channel
 from .lib import create_tables, get_session
 
-__all__ = ['Network', 'User', 'get_session', 'create_tables']
+__all__ = ['Network', 'User', 'Channel', 'get_session', 'create_tables']
---
+++ ircb/models/channel.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from sqlalchemy import (Column, String, Integer, ForeignKey, DateTime,
+                        UniqueConstraint)
+from sqlalchemy_utils import ChoiceType
+
+from ircb.config import settings
+from ircb.models.lib import Base
+
+CHANNEL_STATUS_TYPES = (
+    (0, 'Connecting'),
+    (1, 'Connected'),
+    (2, 'Disconnecting'),
+    (3, 'Disconnected')
+)
+
+
+class Channel(Base):
+    __tablename__ = 'channels'
+    __table_args__ = (
+        UniqueConstraint('network_id', 'name'),
+    )
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    password = Column(String(100), nullable=False, default='')
+    network_id = Column(Integer(),
+                        ForeignKey('networks.id', ondelete='CASCADE'),
+                        nullable=False)
+    user_id = Column(Integer(),
+                     ForeignKey('users.id', ondelete='CASCADE'),
+                     nullable=False)
+
+    # Runtime fields
+    status = Column(ChoiceType(CHANNEL_STATUS_TYPES))
+
+    # timestamps
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    last_updated = Column(DateTime, default=datetime.datetime.utcnow)
+
