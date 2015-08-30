ae8c2146c11b39b1015768aefbc81fc7297aa0d4
Ratnadeep Debnath <rtnpro@gmail.com>
2015-08-30 13:18:42 +0530

    Added initial signal names as constants.
    These signals will be used for publishing and subscribing to messages from
    a central dispatcher.

---
+++ ircb/lib/constants/signals.py
@@ -0,0 +1,15 @@
+# IRC Network
+NETWORK_CONNECT = 'network-connect'
+NETWORK_DISCONNECT = 'network-disconnect'
+NETWORK_DISCONNECTED = 'network-disconnected'
+NETWORK_CONNECTED = 'network-connected'
+
+# Stores
+
+# Network store
+STORE_NETWORK_CREATE = 'store-network-create'
+STORE_NETWORK_CREATED = 'store-network-created'
+STORE_NETWORK_UPDATE = 'store-network-update'
+STORE_NETWORK_UPDATED = 'store-network-updated'
+STORE_NETWORK_DELETE = 'store-network-delete'
+STORE_NETWORK_DELETED = 'store-network-deleted'
