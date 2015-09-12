808b76d92680b6b40c68dddbf82e4b49315ae730
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2015-09-12 23:22:15 +0530

    install dependencies in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ try:
 except:
     from distutils.core import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().spitlines()
+
 setup(
     name='ircb',
     version='0.0.1',
@@ -15,6 +18,7 @@ setup(
     author='Ratnadeep Debnath',
     author_email='rtnpro@gmail.com',
     license='MIT',
+    install_requires=requirements,
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
