e89767d61dd88df4cf90912a4c0e884bb452c1ed
Ratnadeep Debnath <rtnpro@gmail.com>
2015-12-11 21:55:00 +0530

    Added runserver command in ircb CLI

--- ircb/cli/main.py
+++ ircb/cli/main.py
@@ -2,6 +2,7 @@ import click
 
 from ircb.cli.user import user_cli
 from ircb.cli.network import network_cli
+from ircb.cli.runserver import runserver_cli
 import ircb.stores
 
 
@@ -13,6 +14,7 @@ def cli():
 
 cli.add_command(user_cli)
 cli.add_command(network_cli)
+cli.add_command(runserver_cli)
 
 if __name__ == '__main__':
     cli()
