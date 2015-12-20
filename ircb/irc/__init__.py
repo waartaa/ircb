935ac269d01d6cd6807066ee055905e29e6c643b
Ratnadeep Debnath <rtnpro@gmail.com>
2015-12-20 19:30:42 +0530

    Fix marking user as away. #32
    - Fix regex for parsing IRC raw messages, allow for 0 or more whitespaces before 'msg' group
    - Fix call to irc3.IrcBot.away()

--- ircb/irc/__init__.py
+++ ircb/irc/__init__.py
@@ -84,7 +84,7 @@ class IrcbBot(irc3.IrcBot):
         connection=IrcbIrcConnection
     )
     cmd_regex = re.compile(
-        r'(?P<cmd>[A-Z]+)(?:\s+(?P<args>[^\:]+))?(?:\:(?P<msg>.*))?')
+        r'(?P<cmd>[A-Z]+)(?:\s+(?P<args>[^\:]+))?(?:\s*\:(?P<msg>.*))?')
 
     def __init__(self, *args, **kwargs):
         self.clients = None
@@ -165,7 +165,7 @@ class IrcbBot(irc3.IrcBot):
         elif cmd == 'TOPIC':
             self.topic(args, msg)
         elif cmd == 'AWAY':
-            self.away(args, msg)
+            self.away(msg)
         elif cmd == 'QUIT':
             self.quit(msg)
         elif cmd == 'NICK':
