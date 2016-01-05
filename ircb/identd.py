3508fa23dde567210efda669544188dd4e85d7c7
Ratnadeep Debnath <rtnpro@gmail.com>
2016-01-05 18:38:45 +0530

    Implement working identd server.

--- ircb/identd.py
+++ ircb/identd.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import asyncio
+import logging
 
+import ircb.stores
 from ircb.storeclient import NetworkStore
 
+logger = logging.getLogger('identd')
+
+"""
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -20,3 +26,47 @@ while True:
     result = yield from NetworkStore.get({'query':{'lport': lport,
                                                    'rport': rport}})
     print(result)
+"""
+
+
+class IdentdServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        print('New client connection received')
+        self.transport = transport
+
+    def data_received(self, data):
+        print(data)
+
+
+class IdentdServer(object):
+
+    def __init__(self, loop=None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        self.loop = loop
+
+    def create(self, host, port):
+        coro = self.loop.create_server(IdentdServerProtocol,
+                                       host,
+                                       port)
+        logger.info('Listening on {}:{}'.format(host, port))
+        return self.loop.run_until_complete(coro)
+
+    def start(self, host, port):
+        server = self.create(host, port)
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+        server.close()
+        self.loop.run_until_complete(server.wait_closed())
+
+
+def runserver(host='0.0.0.0', port=113):
+    logging.config.dictConfig(settings.LOGGING_CONF)
+    ircb.stores.initialize()
+    identd_server = IdentdServer()
+    identd_server.start()
+
+if __name__ == '__main__':
+    runserver()
