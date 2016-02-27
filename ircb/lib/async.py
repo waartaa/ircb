8ca1d55987dd1cc7c004280a675883e2a8b33129
Ratnadeep Debnath <rtnpro@gmail.com>
2016-02-27 19:36:43 +0530

    Don't run process_queue in Dispatcher as a long running task.
    Rather, we trigger it when we have items in the queue. This is done to avoid keeping
    tasks pending when closing the event loop.

--- ircb/lib/async.py
+++ ircb/lib/async.py
@@ -6,5 +6,7 @@ def coroutinize(func):
         loop = asyncio.get_event_loop()
         coro = asyncio.coroutine(func)
         loop.run_until_complete(coro(*args, **kwargs))
+        from ircb.storeclient.base import dispatcher
+        loop.run_until_complete(dispatcher.stop())
         loop.close()
     return wrapper
