a6435dc765ea8f27bf95900b9cef68f81d399497
Ratnadeep Debnath <rtnpro@gmail.com>
2016-03-23 23:42:19 +0530

    Added view to list IRC networks for a logged in user.

--- ircb/web/app.py
+++ ircb/web/app.py
@@ -9,6 +9,7 @@ from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 from ircb.config import settings
 from ircb.web.user import SigninView, SignoutView
+from ircb.web.network import NetworkListView
 
 logging.config.dictConfig(settings.LOGGING_CONF)
 
@@ -34,6 +35,8 @@ def init(loop):
 
     app.router.add_route('*', '/api/signin', SigninView, name='signin')
     app.router.add_route('*', '/api/signout', SignoutView, name='signout')
+    app.router.add_route('*', '/api/networks', NetworkListView,
+                         name='networks')
     srv = yield from loop.create_server(
         app.make_handler(), '0.0.0.0', 10001)
     return srv
---
+++ ircb/web/network.py
@@ -0,0 +1,25 @@
+import asyncio
+import json
+
+from aiohttp import web
+from aiohttp_auth.auth import get_auth
+from ircb.web.decorators import auth_required
+from ircb.web.lib import View
+from ircb.storeclient import UserStore, NetworkStore
+
+
+class NetworkListView(View):
+    store = NetworkStore
+    datetime_fields = ['created', 'last_updated']
+
+    @auth_required
+    @asyncio.coroutine
+    def get(self):
+        username = yield from get_auth(self.request)
+        user = yield from UserStore.get(
+            dict(query=('username', username)))
+        networks = yield from NetworkStore.get(
+            dict(query={'user_id': user.id}))
+        return web.Response(body=self.serialize(networks).encode(),
+                            content_type='application/json')
+
