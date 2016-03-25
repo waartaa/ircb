9fb31cbdd95885a434a1f55adcdd802fb6689f02
Ratnadeep Debnath <rtnpro@gmail.com>
2016-03-25 10:34:16 +0530

    Cleaned up network model definition.

--- ircb/models/network.py
+++ ircb/models/network.py
@@ -3,8 +3,7 @@
 import datetime
 from hashlib import md5
 
-from sqlalchemy import (Column, String, Integer, ForeignKey, DateTime,
-                        UniqueConstraint, Boolean)
+import sqlalchemy as sa
 from sqlalchemy_utils import ChoiceType, Choice
 
 from ircb.models.lib import Base, get_session
@@ -30,51 +29,52 @@ def _create_access_token(user_id, network_name):
     return md5('{}{}{}{}'.format(settings.SECRET_KEY,
                                  user.username,
                                  network_name,
-                                 datetime.datetime.utcnow()).encode()
+                                 sa.DateTime.sa.DateTime.utcnow()).encode()
                ).hexdigest()
 
 
 class Network(Base):
     __tablename__ = 'networks'
     __table_args__ = (
-        UniqueConstraint('user_id', 'name'),
+        sa.UniqueConstraint('user_id', 'name'),
     )
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    nickname = Column(String(20), nullable=False)
-    hostname = Column(String(100), nullable=False)
-    port = Column(Integer, nullable=False)
-    realname = Column(String(100), nullable=False, default='')
-    username = Column(String(50), nullable=False, default='')
-    password = Column(String(100), nullable=False, default='')
-    usermode = Column(String(1), nullable=False, default='0')
-    ssl = Column(Boolean(), default=False)
-    ssl_verify = Column(ChoiceType(SSL_VERIFY_CHOICES),
-                        default=Choice(*SSL_VERIFY_CHOICES[0]))
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(255), nullable=False)
+    nickname = sa.Column(sa.String(20), nullable=False)
+    hostname = sa.Column(sa.String(100), nullable=False)
+    port = sa.Column(sa.Integer, nullable=False)
+    realname = sa.Column(sa.String(100), nullable=False, default='')
+    username = sa.Column(sa.String(50), nullable=False, default='')
+    password = sa.Column(sa.String(100), nullable=False, default='')
+    usermode = sa.Column(sa.String(1), nullable=False, default='0')
+    ssl = sa.Column(sa.Boolean(), default=False)
+    ssl_verify = sa.Column(ChoiceType(SSL_VERIFY_CHOICES),
+                           default=Choice(*SSL_VERIFY_CHOICES[0]))
 
-    access_token = Column(String(100), nullable=False, unique=True,
-                          default=lambda context: _create_access_token(
-                          context.current_parameters['user_id'],
-                          context.current_parameters['name']))
-    user_id = Column(
-        Integer(), ForeignKey('users.id', ondelete='CASCADE'),
+    access_token = sa.Column(sa.String(100), nullable=False, unique=True,
+                             default=lambda context: _create_access_token(
+                             context.current_parameters['user_id'],
+                             context.current_parameters['name']))
+    user_id = sa.Column(
+        sa.Integer(), sa.ForeignKey(User.id, ondelete='CASCADE'),
         nullable=False)
 
     # Runtime fields
-    current_nickname = Column(String(20), nullable=True)
-    status = Column(ChoiceType(NETWORK_STATUS_TYPES))
+    current_nickname = sa.Column(sa.String(20), nullable=True)
+    status = sa.Column(ChoiceType(NETWORK_STATUS_TYPES))
 
     # Remote socket info
-    rhost = Column(String(100), nullable=True)
-    rport = Column(Integer(), nullable=True)
+    rhost = sa.Column(sa.String(100), nullable=True)
+    rport = sa.Column(sa.Integer(), nullable=True)
 
     # Local socket info
-    lhost = Column(String(100), nullable=True)
-    lport = Column(Integer(), nullable=True)
+    lhost = sa.Column(sa.String(100), nullable=True)
+    lport = sa.Column(sa.Integer(), nullable=True)
 
     # timestamps
-    created = Column(DateTime, default=datetime.datetime.utcnow)
-    last_updated = Column(DateTime, default=datetime.datetime.utcnow)
+    created = sa.Column(sa.DateTime, default=datetime.datetime.utcnow)
+    last_updated = sa.Column(sa.DateTime,
+                             default=datetime.datetime.utcnow)
 
     def create_access_token(self):
         return _create_access_token(self.user.id, self.name)
