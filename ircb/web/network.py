b83c48865b9795571e1680aab9ce46d8f2589892
Ratnadeep Debnath <rtnpro@gmail.com>
2016-03-26 21:32:51 +0530

    Add network update API.
    Use WTForms form validating POST/PUT data.

--- ircb/web/network.py
+++ ircb/web/network.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 
-from aiohttp import web
+from aiohttp import web, MultiDict
 from aiohttp_auth.auth import get_auth
 from ircb.web.decorators import auth_required
 from ircb.web.lib import View
@@ -9,7 +9,38 @@ from ircb.storeclient import UserStore, NetworkStore
 from ircb.forms import NetworkForm
 
 
-class NetworkListView(View):
+class NetworkViewMixin(object):
+
+    @asyncio.coroutine
+    def _create_or_update(self, data, username, network=None):
+        if network:
+            network_data = network.to_dict()
+            network_data.update(data)
+            form = NetworkForm(formdata=MultiDict(network_data))
+        else:
+            form = NetworkForm(formdata=MultiDict(data))
+        form.validate()
+        if form.errors:
+            return web.Response(
+                body=json.dumps(form.errors).encode(),
+                status=400,
+                content_type='application/json')
+        cleaned_data = form.data
+        cleaned_data['user'] = username
+        if network:
+            network = yield from NetworkStore.update(
+                dict(
+                    filter=('id', network.id),
+                    update=cleaned_data
+                )
+            )
+        else:
+            network = yield from NetworkStore.create(**cleaned_data)
+        return web.Response(body=self.serialize(network).encode(),
+                            content_type='application/json')
+
+
+class NetworkListView(View, NetworkViewMixin):
     store = NetworkStore
 
     @auth_required
@@ -28,26 +59,12 @@ class NetworkListView(View):
     def post(self):
         username = yield from get_auth(self.request)
         data = yield from self.request.post()
-        form = NetworkForm(**data)
-        form.validate()
-        if form.errors:
-            return web.Response(
-                body=json.dumps(form.errors).encode(),
-                status=400,
-                content_type='application/json')
-        cleaned_data = form.data
-        cleaned_data['user'] = username
-        network = yield from NetworkStore.create(**cleaned_data)
-        return web.Response(body=self.serialize(network).encode(),
-                            content_type='application/json')
+        resp = yield from self._create_or_update(data, username)
+        return resp
 
 
-class NetworkView(View):
+class NetworkView(View, NetworkViewMixin):
     store = NetworkStore
-    w_fields = ['name', 'nickname', 'hostname', 'port', 'realname',
-                'username', 'password', 'usermode', 'ssl',
-                'ssl_verify']
-    post_reqd_fields = ['user', 'name', 'host', 'port', 'nick']
 
     @auth_required
     @asyncio.coroutine
@@ -63,3 +80,19 @@ class NetworkView(View):
             raise web.HTTPNotFound()
         return web.Response(body=self.serialize(network).encode(),
                             content_type='application/json')
+
+    @auth_required
+    @asyncio.coroutine
+    def put(self):
+        network_id = self.request.match_info['id']
+        username = yield from get_auth(self.request)
+        data = yield from self.request.post()
+        user = yield from UserStore.get(
+            dict(query=('username', username)))
+        networks = yield from NetworkStore.get(
+            dict(query={'user_id': user.id, 'id': network_id})
+        )
+        if not networks:
+            raise web.HTTPNotFound()
+        resp = yield from self._create_or_update(data, username, networks[0])
+        return resp
