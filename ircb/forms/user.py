97182313f2471ff0e895f9f4efd66b08f7939c44
Ratnadeep Debnath <rtnpro@gmail.com>
2016-03-28 13:52:04 +0530

    Added user form.

---
+++ ircb/forms/user.py
@@ -0,0 +1,16 @@
+from wtforms_alchemy import ModelForm
+
+from ircb.models import User
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        only = [
+            'username',
+            'email',
+            'password',
+            'first_name',
+            'last_name'
+        ]
+        unique_validator = None
