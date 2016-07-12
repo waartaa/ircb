bebf7d7e0d6dccf4d848fa3cd4367b0d682697ff
Ratnadeep Debnath <rtnpro@gmail.com>
2016-07-12 10:41:31 +0530

    Add realtime publisher for channels. Fixes #74

--- ircb/publishers/__init__.py
+++ ircb/publishers/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from ircb.publishers.logs import MessageLogPublisher
 from ircb.publishers.networks import NetworkPublisher
+from ircb.publishers.channels import ChannelPublisher
 
 if __name__ == '__main__':
     import asyncio
@@ -16,11 +17,15 @@ if __name__ == '__main__':
     except:
         print("Usage: __init__.py '<hostname>' '<roomname>' '<user_id>'")
         sys.exit(1)
+
     message_log_pub = MessageLogPublisher(hostname, roomname, int(user_id))
     message_log_pub.run()
 
     network_pub = NetworkPublisher(int(user_id))
     network_pub.run()
+
+    channel_pub = ChannelPublisher(int(user_id))
+    channel_pub.run()
 
     loop = asyncio.get_event_loop()
     loop.run_forever()
